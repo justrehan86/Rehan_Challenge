3a205a055945d8fde7996272dae7c27ad7abfb06
justrehan86 <justrehan86@github.com>
2019-03-05 12:38:58 -0500

    creditcardquick

--- creditcard.py
+++ creditcard.py
@@ -2,19 +2,11 @@ import re
 
 c = input()
 b=0
-pattern1 = r"[4-6]{1}[0-9]{3}\-[0-9]{4}\-[0-9]{4}\-[0-9]{4}|[4-6]{1}[0-9]{15}"
+pattern1 = "[4-6]{1}[0-9]{3}\-[0-9]{4}\-[0-9]{4}\-[0-9]{4}|[4-6]{1}[0-9]{15}|(\d)\1{3}"
 m=re.match(pattern1,c)
-pattern2 = r'(\d)\1{3}'
-n=re.search(pattern2,c)
-while b<1 :
-  if n:
-    print("invalid")
-    break
-  else:
-   print("")
-  if m:
+if m:
     print("valid")
-  else:
+else:
     print("invalid")
-  b+=1  
+    
 

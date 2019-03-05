1a917da91679b16dd4574eb2b70289669f89e10c
justrehan86 <justrehan86@github.com>
2019-03-05 12:24:52 -0500

    this is for challenge

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ variable "key_path" {
 
 variable "localip" {
 
-  default = "192.168.0.0"
+  default = "192.168.1.0"
 }
   
 
@@ -16,13 +16,13 @@ provider "aws" {
   profile = "rehan"
   }
 
- resource "aws_key_pair" "wipro1" {
-    key_name   = "wipro1"
+ resource "aws_key_pair" "rehan1" {
+    key_name   = "rehan1"
     public_key = "${file(var.key_path)}"
     }
 
 
-  resource "aws_security_group" "wipro_sg" {
+  resource "aws_security_group" "rehan_sg" {
     name        = "wp_dev_sg"
     description = "Used for access to the dev instance"
 
@@ -60,11 +60,11 @@ provider "aws" {
     }
     }
 
-  resource "aws_instance" "wipro-demo" {
+  resource "aws_instance" "rehan-demo" {
     instance_type = "t2.micro"
     ami           = "ami-02da3a138888ced85"
-    vpc_security_group_ids = ["${aws_security_group.wipro_sg.id}"]
-    key_name = "${aws_key_pair.wipro1.id}"
+    vpc_security_group_ids = ["${aws_security_group.rehan_sg.id}"]
+    key_name = "${aws_key_pair.rehan1.id}"
     user_data = <<-EOF
     #!/bin/bash
     sudo yum install -y httpd 2> var/log/error1.txt
@@ -74,9 +74,43 @@ provider "aws" {
     EOF
 
     tags {
-    Name = "wipro-demo"
+    Name = "rehan-demo"
     }
   
   
     }
+
+    resource "aws_elb" "wp_elb" {
+  name = "rehan-elb"
+
+  subnets = ["${aws_subnet.wp_public1_subnet.id}",
+    "${aws_subnet.wp_public2_subnet.id}",
+  ]
+
+  security_groups = ["${aws_security_group.wp_public_sg.id}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = "${var.elb_healthy_threshold}"
+    unhealthy_threshold = "${var.elb_unhealthy_threshold}"
+    timeout             = "${var.elb_timeout}"
+    target              = "TCP:80"
+    interval            = "${var.elb_interval}"
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags {
+    Name = "wp_${var.domain_name}-elb"
+  }
+}
 
